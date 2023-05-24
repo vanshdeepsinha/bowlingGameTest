09fd558c8899ec298e1d3a0451621fc271793083
vanshdeep <vanshdeepsinha@gmail.com>
2023-05-24 16:06:03 +0530

    Initial set up of gitignore

---
+++ src/main/java/com/test/bowlinggame/BowlinggameApplication.java
@@ -0,0 +1,13 @@
+package com.test.bowlinggame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BowlinggameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BowlinggameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/bowlinggame/BowlinggameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.bowlinggame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BowlinggameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
