bb2f36f2b5f61aa42b23b487bbcc56f418830f16
vanshdeep <vanshdeepsinha@gmail.com>
2023-05-25 15:35:57 +0530

    Game: get request from user, GameOutput: produces game score and message

---
+++ src/main/java/com/test/bowlinggame/entity/Game.java
@@ -0,0 +1,14 @@
+package com.test.bowlinggame.entity;
+
+public class Game {
+	
+	 private String bowlingGame;
+
+	 public String getBowlingGame() {
+		 return bowlingGame;
+	 }
+
+	 public void setBowlingGame(String bowlingGame) {
+		 this.bowlingGame = bowlingGame;
+	 }
+}
---
+++ src/main/java/com/test/bowlinggame/entity/GameOutput.java
@@ -0,0 +1,52 @@
+package com.test.bowlinggame.entity;
+
+public class GameOutput {
+	
+	 private Integer score;
+	 private Integer code;
+	 private String message;
+	 private Integer turkeys;
+	 int[] frameScores = new int[10];
+	 
+	 
+	 public Integer getScore() {
+			return score;
+	 }
+	 
+	 public void setScore(Integer score) {
+			this.score = score;
+	 }
+	 
+	 public Integer getCode() {
+			return code;
+	  }
+	 
+	 public void setCode(Integer code) {
+			this.code = code;
+	  }
+	 
+	 public String getMessage() {
+			return message;
+	  }
+	 
+	 public void setMessage(String message) {
+		    this.message = message;
+	  }
+	 
+	 public Integer getTurkeys() {
+			return turkeys;
+	  }
+	 
+	 public void setTurkeys(Integer turkeys) {
+			this.turkeys = turkeys;
+	  }
+	 
+	 public int[] getFrameScores() {
+			return frameScores;
+	  }
+	 
+	 public void setFrameScores(int[] frameScores) {
+			this.frameScores = frameScores;
+	  }
+	 
+}
